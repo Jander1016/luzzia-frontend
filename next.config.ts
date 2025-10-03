118d786a7621be402bbc8d2673398bda8b85c1af
Jander Gomez <jandergb.30gmail.com>
2025-10-03 09:51:05 +0200

    feat: update Next.js to v15.5.4 with TypeScript

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // desactiva el sistema de optimización
+  },
+  reactStrictMode: true,
+  experimental: {
+    reactCompiler: {
+      // Configuración básica para React Compiler
+      compilationMode: 'annotation'
+    }
+  },
 };
 
 export default nextConfig;
